da8b279fe8412de2b0281c7b3d353704b4a6fa06
GitHub Action <action@github.com>
2025-07-20 14:06:14 +0000

    chore: bump version to v0.1.1 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.flx_apps.digitaldetox"
         minSdk = 26
         targetSdk = 33
-        versionCode = 100
-        versionName = "0.1.0"
+        versionCode = 101
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
