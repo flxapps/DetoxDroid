9f802c5fa9ce74ad0ffd65269c1c6ce2b889ba89
Felix <mail@flx.es>
2026-09-17 15:09:10 +0200

    chore: update gradle to 9.5.0, agp to 9.3.2 and ksp to 2.3.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,9 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "9.1.1" apply false
+    id("com.android.application") version "9.3.2" apply false
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
-    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
+    id("com.google.devtools.ksp") version "2.3.6" apply false
     // DI
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
