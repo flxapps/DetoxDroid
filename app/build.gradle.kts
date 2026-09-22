3a4b29e40d5d735cac50abbb583b5cf1eeb85eeb
GitHub Action <action@github.com>
2026-09-22 22:42:04 +0000

    chore: bump version to v2.8.2 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.flx_apps.digitaldetox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20801
-        versionName = "2.8.1"
+        versionCode = 20802
+        versionName = "2.8.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
